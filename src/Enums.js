d7603b011a1ef16294ee5f9d98e937c862f755a9
Bård Rotzer <bard.ingebricson@gmail.com>
2017-05-15 14:35:47 +0200

    Add ANCHOR_TOP to enums

--- src/Enums.js
+++ src/Enums.js
@@ -6,6 +6,7 @@ export default class Enums {
   static get ANCHOR_LEFT () { return 'left'; }
   static get ANCHOR_RIGHT () { return 'right';}
   static get ANCHOR_BOTTOM () {return 'bottom';}
+  static get ANCHOR_TOP () {return 'top';}
   static get LABEL_INSIDE () {return 'inside';}
   static get LABEL_OUTSIDE () {return 'outside';}
   static get LABEL_FIT () {return 'fit';}         // TODO: Not in use ??!
